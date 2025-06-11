ad33976245a8261017c42dfbcf011f86d85e2927
Tanitsin <tanitsin.don@student.mahidol.edu>
2025-06-11 11:02:54 +0700

    ..

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,9 @@ Route::get('/', function () {
     return redirect()->route('drivers.index');
 });
 
+Route::get('/we', function () {
+    return view('welcome');
+});
+
 Route::resource('drivers', App\Http\Controllers\DriverController::class);
 Route::put('drivers/{driver}/approve', [App\Http\Controllers\DriverController::class, 'approve'])->name('drivers.approve');
