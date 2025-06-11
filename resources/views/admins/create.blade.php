f93181af6b6fb8b48c5992698d5c1024ee3d96ce
Tanitsin <tanitsin.don@student.mahidol.edu>
2025-06-11 16:07:45 +0700

    delete add_Super_Admin

--- resources/views/admins/create.blade.php
+++ resources/views/admins/create.blade.php
@@ -22,10 +22,7 @@
             <label class="form-label">Password</label>
             <input type="password" name="password" class="form-control" required>
         </div>
-        <div class="form-check mb-3">
-            <input class="form-check-input" type="checkbox" name="is_super" id="is_super">
-            <label class="form-check-label" for="is_super">Super Admin</label>
-        </div>
+
         <button type="submit" class="btn btn-primary">Save</button>
     </form>
 </div>
