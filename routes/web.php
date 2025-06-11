716900930cedc6a670f236fac73ec67190f22442
Tanitsin <tanitsin.don@student.mahidol.edu>
2025-06-11 14:56:36 +0700

    redirect  welcome

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminAuthController;
 
 Route::get('/', function () {
-    return redirect()->route('drivers.index');
+    return redirect()->route('admin.login');
 });
 
 Route::get('/we', function () {
