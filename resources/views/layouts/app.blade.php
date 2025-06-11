62e76a713db9162cb0e9cd1c992ef4cec3faed6c
Tanitsin <108443598+ZEN747K@users.noreply.github.com>
2025-06-11 11:45:52 +0700

    Show admin email in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,9 @@
                 <ul class="navbar-nav ms-auto">
                     @auth('admin')
                     <li class="nav-item">
+                        <span class="navbar-text">{{ auth('admin')->user()->email }}</span>
+                    </li>
+                    <li class="nav-item">
                         <form method="POST" action="{{ route('admin.logout') }}">
                             @csrf
                             <button type="submit" class="btn btn-link nav-link" style="display:inline; padding:0;">Logout</button>
