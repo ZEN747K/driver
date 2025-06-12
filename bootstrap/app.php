8cc7c5429a1d01aa44c56303e7e995f651aad455
Tanitsin <108443598+ZEN747K@users.noreply.github.com>
2025-06-12 09:44:43 +0700

    Fix middleware alias usage

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias('verify.admin.credentials', VerifyAdminCredentials::class);
+        $middleware->alias([
+            'verify.admin.credentials' => VerifyAdminCredentials::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
